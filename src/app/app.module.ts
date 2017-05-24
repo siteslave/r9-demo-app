09b752bb84ed1a26802c89fd05cbaba52c94095a
Satit Rianpit <rianpit@gmail.com>
2017-05-24 17:10:40 +0700

    add AuthHttp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { HttpModule } from '@angular/http';
+import { HttpModule, Http } from '@angular/http';
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -15,6 +16,14 @@ import { SettingPage } from '../pages/setting/setting';
 import { AboutPage } from '../pages/about/about';
 import { TabsPage } from '../pages/tabs/tabs';
 import { UserProvider } from '../providers/user/user';
+
+export function getAuthHttp(http) {
+  return new AuthHttp(new AuthConfig({
+    noJwtError: true,
+    globalHeaders: [{ 'Accept': 'application/json' }],
+    tokenGetter: (() => localStorage.getItem('token')),
+  }), http);
+}
 
 @NgModule({
   declarations: [
@@ -50,6 +59,11 @@ import { UserProvider } from '../providers/user/user';
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     { provide: 'API_URL', useValue: 'http://localhost:3000' },
+    {
+      provide: AuthHttp,
+      useFactory: getAuthHttp,
+      deps: [Http]
+    },
     UserProvider
   ]
 })
