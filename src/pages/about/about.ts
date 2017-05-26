afb64999e8f8a59fde844ebaf3b2813c32e852ea
Satit Rianpit <rianpit@gmail.com>
2017-05-26 10:12:36 +0700

    Browse picture

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -45,6 +45,20 @@ export class AboutPage {
   }
 
   browsePicture() {
+    const options: CameraOptions = {
+      quality: 60,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }
 
+    this.camera.getPicture(options)
+      .then((imageData) => {
+      this.base64Preview = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Handle error
+      console.log(err);
+    });
   }
 }
